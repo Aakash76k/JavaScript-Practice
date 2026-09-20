e21de8d8228be48cf3298d5a1a3f8dfb271e8e88
Aakash <aakash76k747@gmail.com>
2026-09-20 22:49:59 +0530

    update  a project quiz app using html,css , js

--- Quiz-App/script.js
+++ Quiz-App/script.js
@@ -1,19 +1,15 @@
-
 const studentForm = document.getElementById("studentForm");
-
+const quizSection = document.getElementById("quizSection");
 studentForm.addEventListener("submit", function (e) {
   e.preventDefault();
-
-  console.log("Form Submitted");
-
   const name = document.getElementById("name").value;
   const email = document.getElementById("email").value;
   const phone = document.getElementById("phone").value;
   const course = document.getElementById("course").value;
-
-  console.log(name);
-  console.log(email);
-  console.log(phone);
-  console.log(course);
+  console.log("Name:", name);
+  console.log("Email:", email);
+  console.log("Phone:", phone);
+  console.log("Course:", course);
+  //studentForm.parentElement.style.display = "none";
+  quizSection.style.display = "block";
 });
-
